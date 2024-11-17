5888491f2fb6d71f69ee4b0fe3c56ed384dac96c
jdalton <john.david.dalton@gmail.com>
2024-11-17 15:18:42 -0500

    Change formatting from 4 spaces to 2 spaces

--- eslint.config.js
+++ eslint.config.js
@@ -18,86 +18,86 @@ const gitignorePath = path.resolve(rootPath, GIT_IGNORE)
 const prettierignorePath = path.resolve(rootPath, PRETTIER_IGNORE)
 
 module.exports = [
-    includeIgnoreFile(gitignorePath),
-    includeIgnoreFile(prettierignorePath),
-    {
-        files: ['**/*.{c,}js'],
-        ...importXPlugin.flatConfigs.recommended,
-        languageOptions: {
-            ...importXPlugin.flatConfigs.recommended.languageOptions,
-            ecmaVersion: 'latest',
-            sourceType: 'script'
-        },
-        rules: {
-            ...importXPlugin.flatConfigs.recommended.rules,
-            'import-x/no-named-as-default-member': 'off',
-            'import-x/no-unresolved': ['error', { commonjs: true }],
-            'import-x/order': [
-                'warn',
-                {
-                    groups: [
-                        'builtin',
-                        'external',
-                        'internal',
-                        ['parent', 'sibling', 'index'],
-                        'type'
-                    ],
-                    pathGroups: [
-                        {
-                            pattern: '@socket{registry,security}/**',
-                            group: 'internal'
-                        }
-                    ],
-                    pathGroupsExcludedImportTypes: ['type'],
-                    'newlines-between': 'always',
-                    alphabetize: {
-                        order: 'asc'
-                    }
-                }
-            ]
-        }
+  includeIgnoreFile(gitignorePath),
+  includeIgnoreFile(prettierignorePath),
+  {
+    files: ['**/*.{c,}js'],
+    ...importXPlugin.flatConfigs.recommended,
+    languageOptions: {
+      ...importXPlugin.flatConfigs.recommended.languageOptions,
+      ecmaVersion: 'latest',
+      sourceType: 'script'
     },
-    {
-        files: ['scripts/**/*.js', 'test/**/*.cjs'],
-        ...nodePlugin.configs['flat/recommended-script']
-    },
-    {
-        files: ['scripts/**/*.js', 'test/**/*.cjs'],
-        plugins: {
-            'sort-destructure-keys': sortDestructureKeysPlugin,
-            unicorn: unicornPlugin
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            'n/exports-style': ['error', 'module.exports'],
-            // The n/no-unpublished-bin rule does does not support non-trivial glob
-            // patterns used in package.json "files" fields. In those cases we simplify
-            // the glob patterns used.
-            'n/no-unpublished-bin': ['error'],
-            'n/no-unsupported-features/es-builtins': ['error'],
-            'n/no-unsupported-features/es-syntax': ['error'],
-            'n/no-unsupported-features/node-builtins': [
-                'error',
-                {
-                    ignores: ['test', 'test.describe'],
-                    // Lazily access constants.maintainedNodeVersions.
-                    version: constants.maintainedNodeVersions.previous
-                }
-            ],
-            'n/prefer-node-protocol': ['error'],
-            'no-await-in-loop': ['error'],
-            'no-control-regex': ['error'],
-            'no-empty': ['error', { allowEmptyCatch: true }],
-            'no-new': ['error'],
-            'no-proto': ['error'],
-            'no-unused-vars': [
-                'error',
-                { argsIgnorePattern: '^_|^this$', ignoreRestSiblings: true }
-            ],
-            'no-warning-comments': ['warn', { terms: ['fixme'] }],
-            'sort-destructure-keys/sort-destructure-keys': ['error'],
-            'sort-imports': ['error', { ignoreDeclarationSort: true }],
-            'unicorn/consistent-function-scoping': ['error']
+    rules: {
+      ...importXPlugin.flatConfigs.recommended.rules,
+      'import-x/no-named-as-default-member': 'off',
+      'import-x/no-unresolved': ['error', { commonjs: true }],
+      'import-x/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'type'
+          ],
+          pathGroups: [
+            {
+              pattern: '@socket{registry,security}/**',
+              group: 'internal'
+            }
+          ],
+          pathGroupsExcludedImportTypes: ['type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc'
+          }
         }
+      ]
     }
+  },
+  {
+    files: ['scripts/**/*.js', 'test/**/*.cjs'],
+    ...nodePlugin.configs['flat/recommended-script']
+  },
+  {
+    files: ['scripts/**/*.js', 'test/**/*.cjs'],
+    plugins: {
+      'sort-destructure-keys': sortDestructureKeysPlugin,
+      unicorn: unicornPlugin
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'n/exports-style': ['error', 'module.exports'],
+      // The n/no-unpublished-bin rule does does not support non-trivial glob
+      // patterns used in package.json "files" fields. In those cases we simplify
+      // the glob patterns used.
+      'n/no-unpublished-bin': ['error'],
+      'n/no-unsupported-features/es-builtins': ['error'],
+      'n/no-unsupported-features/es-syntax': ['error'],
+      'n/no-unsupported-features/node-builtins': [
+        'error',
+        {
+          ignores: ['test', 'test.describe'],
+          // Lazily access constants.maintainedNodeVersions.
+          version: constants.maintainedNodeVersions.previous
+        }
+      ],
+      'n/prefer-node-protocol': ['error'],
+      'no-await-in-loop': ['error'],
+      'no-control-regex': ['error'],
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-new': ['error'],
+      'no-proto': ['error'],
+      'no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_|^this$', ignoreRestSiblings: true }
+      ],
+      'no-warning-comments': ['warn', { terms: ['fixme'] }],
+      'sort-destructure-keys/sort-destructure-keys': ['error'],
+      'sort-imports': ['error', { ignoreDeclarationSort: true }],
+      'unicorn/consistent-function-scoping': ['error']
+    }
+  }
 ]
--- src/encode.js
+++ src/encode.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const {
-    REUSED_SEARCH_PARAMS,
-    REUSED_SEARCH_PARAMS_KEY,
-    REUSED_SEARCH_PARAMS_OFFSET
+  REUSED_SEARCH_PARAMS,
+  REUSED_SEARCH_PARAMS_KEY,
+  REUSED_SEARCH_PARAMS_OFFSET
 } = require('./constants')
 const { isObject } = require('./objects')
 const { isNonEmptyString } = require('./strings')
@@ -11,62 +11,60 @@ const { isNonEmptyString } = require('./strings')
 const { encodeURIComponent } = globalThis
 
 function encodeNamespace(namespace) {
-    return isNonEmptyString(namespace)
-        ? encodeURIComponent(namespace)
-              .replace(/%3A/g, ':')
-              .replace(/%2F/g, '/')
-        : ''
+  return isNonEmptyString(namespace)
+    ? encodeURIComponent(namespace).replace(/%3A/g, ':').replace(/%2F/g, '/')
+    : ''
 }
 
 function encodeQualifierParam(param) {
-    if (isNonEmptyString(param)) {
-        // Param key and value are encoded with `percentEncodeSet` of
-        // 'application/x-www-form-urlencoded' and `spaceAsPlus` of `true`.
-        // https://url.spec.whatwg.org/#urlencoded-serializing
-        REUSED_SEARCH_PARAMS.set(REUSED_SEARCH_PARAMS_KEY, param)
-        return replacePlusSignWithPercentEncodedSpace(
-            REUSED_SEARCH_PARAMS.toString().slice(REUSED_SEARCH_PARAMS_OFFSET)
-        )
-    }
-    return ''
+  if (isNonEmptyString(param)) {
+    // Param key and value are encoded with `percentEncodeSet` of
+    // 'application/x-www-form-urlencoded' and `spaceAsPlus` of `true`.
+    // https://url.spec.whatwg.org/#urlencoded-serializing
+    REUSED_SEARCH_PARAMS.set(REUSED_SEARCH_PARAMS_KEY, param)
+    return replacePlusSignWithPercentEncodedSpace(
+      REUSED_SEARCH_PARAMS.toString().slice(REUSED_SEARCH_PARAMS_OFFSET)
+    )
+  }
+  return ''
 }
 
 function encodeQualifiers(qualifiers) {
-    if (isObject(qualifiers)) {
-        // Sort this list of qualifier strings lexicographically.
-        const qualifiersKeys = Object.keys(qualifiers).sort()
-        const searchParams = new URLSearchParams()
-        for (let i = 0, { length } = qualifiersKeys; i < length; i += 1) {
-            const key = qualifiersKeys[i]
-            searchParams.set(key, qualifiers[key])
-        }
-        return replacePlusSignWithPercentEncodedSpace(searchParams.toString())
+  if (isObject(qualifiers)) {
+    // Sort this list of qualifier strings lexicographically.
+    const qualifiersKeys = Object.keys(qualifiers).sort()
+    const searchParams = new URLSearchParams()
+    for (let i = 0, { length } = qualifiersKeys; i < length; i += 1) {
+      const key = qualifiersKeys[i]
+      searchParams.set(key, qualifiers[key])
     }
-    return ''
+    return replacePlusSignWithPercentEncodedSpace(searchParams.toString())
+  }
+  return ''
 }
 
 function encodeSubpath(subpath) {
-    return isNonEmptyString(subpath)
-        ? encodeURIComponent(subpath).replace(/%2F/g, '/')
-        : ''
+  return isNonEmptyString(subpath)
+    ? encodeURIComponent(subpath).replace(/%2F/g, '/')
+    : ''
 }
 
 function encodeVersion(version) {
-    return isNonEmptyString(version)
-        ? encodeURIComponent(version).replace(/%3A/g, ':').replace(/%2B/g, '+')
-        : ''
+  return isNonEmptyString(version)
+    ? encodeURIComponent(version).replace(/%3A/g, ':').replace(/%2B/g, '+')
+    : ''
 }
 
 function replacePlusSignWithPercentEncodedSpace(str) {
-    // Convert plus signs to %20 for better portability.
-    return str.replace(/\+/g, '%20')
+  // Convert plus signs to %20 for better portability.
+  return str.replace(/\+/g, '%20')
 }
 
 module.exports = {
-    encodeNamespace,
-    encodeVersion,
-    encodeQualifiers,
-    encodeQualifierParam,
-    encodeSubpath,
-    encodeURIComponent
+  encodeNamespace,
+  encodeVersion,
+  encodeQualifiers,
+  encodeQualifierParam,
+  encodeSubpath,
+  encodeURIComponent
 }
