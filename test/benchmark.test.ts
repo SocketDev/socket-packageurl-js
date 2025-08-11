1bc642d4f268f83be928cd8235a3ca63580fcfdd
jdalton <john.david.dalton@gmail.com>
2025-08-10 23:14:08 -0400

    Switch to vitest

--- test/benchmark.test.ts
+++ test/benchmark.test.ts
@@ -1,15 +1,12 @@
-'use strict'
+import path from 'node:path'
 
-const assert = require('node:assert/strict')
-const path = require('node:path')
-const { describe, it } = require('node:test')
+import { glob } from 'fast-glob'
+import { describe, expect, it } from 'vitest'
 
-const { glob } = require('fast-glob')
+import { readJson } from '@socketsecurity/registry/lib/fs'
+import { isObject } from '@socketsecurity/registry/lib/objects'
 
-const { readJson } = require('@socketsecurity/registry/lib/fs')
-const { isObject } = require('@socketsecurity/registry/lib/objects')
-
-const { PackageURL } = require('../src/package-url')
+import { PackageURL } from '../src/package-url.js'
 
 describe('PackageURL', () => {
   it('Benchmarking the library', async () => {
@@ -24,12 +21,12 @@ describe('PackageURL', () => {
       )
     )
       .filter(Boolean)
-      .flatMap(o => o.tests ?? [])
+      .flatMap((o: any) => o?.tests ?? [])
 
     const iterations = 10000
-    const data = TEST_FILES.filter(obj => isObject(obj.expected_output))
+    const data = TEST_FILES.filter((obj: any) => isObject(obj?.expected_output))
     const { length: dataLength } = data
-    const objects = []
+    const objects: unknown[] = []
     for (let i = 0; i < iterations; i += dataLength) {
       const delta = iterations - (i + dataLength)
       if (delta < 0) {
@@ -41,16 +38,18 @@ describe('PackageURL', () => {
     const start = Date.now()
     for (let i = 0; i < iterations; i += 1) {
       const obj = objects[i]
-      const { expected_output } = obj
-      const purl = new PackageURL(
-        expected_output.type,
-        expected_output.namespace,
-        expected_output.name,
-        expected_output.version,
-        expected_output.qualifiers,
-        expected_output.subpath
-      )
-      PackageURL.fromString(purl.toString())
+      const expected_output = obj?.['expected_output']
+      if (isObject(expected_output)) {
+        const purl = new PackageURL(
+          expected_output.type,
+          expected_output.namespace,
+          expected_output.name,
+          expected_output.version,
+          expected_output.qualifiers,
+          expected_output.subpath
+        )
+        PackageURL.fromString(purl.toString())
+      }
     }
     const end = Date.now()
     console.log(
@@ -58,6 +57,6 @@ describe('PackageURL', () => {
         (end - start) / iterations
       }`
     )
-    assert.ok(end - start > 0)
+    expect(end - start > 0)
   })
 })
